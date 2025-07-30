a4cf8bae225d3b82f72a622d73c7936365441db6
elessandroabreu <binho_abreu@hotmail.com>
2025-07-30 09:38:36 -0300

    Criando + classes

--- src/ItemCarrinho.java
+++ src/ItemCarrinho.java
@@ -1,2 +1,9 @@
 public class ItemCarrinho {
+    Produto produto;
+    int quantidade;
+
+    public ItemCarrinho(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
 }
--- src/Produto.java
+++ src/Produto.java
@@ -1,2 +1,10 @@
 public class Produto {
+    String nome;
+    String categoria;
+    double preco;
+
+    public Produto(String nome, String categoria, double preco) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
 }
